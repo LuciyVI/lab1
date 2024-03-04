82708d34d9fe2bf2f24da847cea149c07ca4cb02
Fedor <f3dorasta@gmail.com>
2024-03-04 15:42:38 +0300

    update check in function

--- lab1.java
+++ lab1.java
@@ -5,64 +5,70 @@ public class lab1{
         public static final String Format = "%.5f%n";
     }
 
-    public static double calc_pi1() {
+    public static void calc_pi1() {
         
         double i=1.0;
 
         double result=1.0;
         
-        int exit = 25;
+        int cyrcle=1;
+         
+        do{        
+                
+                System.out.printf("\n Первое разложение: %.5f%n",result);
+                
+                result-=(1.0/(i+=2)+(1.0/(i+=2)));         
+                if(4*result>Math.PI){
+                cyrcle++;
+                }else {
+                    continue;
+                }
+            }while(Math.abs(result)<Math.PI); 
+    
         
-        do{
         
-            for(int a=1; a<exit; a++) {
         
-               
-            result-=(1.0/(i+=2)+(1.0/(i+=2)));         
-            
-        
-        }; 
-        
-        exit=exit*10;
-        
-        }while(exit<500000);
     
     
-        return 4*result;
+        
 };
   
 
-public static double calc_pi2() {
+public static void calc_pi2() {
+   
     int i=2;
 
     double result=1.0;
     double exit = 2.5;
-    do{
-    for(int a=1; a<exit; a++) {
-    
-           
-        result+=(1.0/((i)*(i=i+1)*(i=i+1))-(1.0/(i)*(i=i+1)*(i=i+1)));         
-        System.out.printf("\n%d",i);
+    boolean flag=false;
+    while(flag){
+        for(int a=1; a<exit; a++) {
+            
+            
+            result=+(1/((i)*(i=+i+1)*(i=+i+1))-(1.0/(i)*(i=+i+1)*(i=+i+1)));         
+            
+        }; 
         
-    }; 
+        System.out.printf("\n Второе разложение %.5f%n",3+4*result);
+        
+        
+    
+    
+    
+    
+    };
+    
+    
 
-    // System.out.printf("\n%d",i+1);
     
-    exit=exit*10;
-    
-    
-    }while(exit<50000);
-    
-    
-
-    return (4+3)*result;
 };
 
     public static void main(String[] args){
             
-    // System.out.printf("%.5f%n \n",calc_pi1());
-    // System.out.printf("%.5f%n \n", calc_pi2());
-     calc_pi2();
+    
+    calc_pi1();
+    calc_pi2(); 
+    
 
     
     };
